282da8a84ae0c66a1f6614a5f698a97c2c420ef4
paurbx <gleroapachii@gmail.com>
2021-12-14 01:52:24 +0300

    all added

---
+++ djforms/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from books.views import create_book, create_book_form, detail_book, mark_sells_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<pk>/', create_book, name='create-book'),
+    path('htmx/create-book-form/', create_book_form, name='create-book-form'),
+    path('htmx/book/<pk>/', detail_book, name="detail-book"),
+    path('htmx/mark_sells/<name>', mark_sells_view, name='mark-sells')
+
+]
